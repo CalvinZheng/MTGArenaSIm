5d850ba1653c7047680c44aee1562ab7439d1f1c
Calvin Zheng <calvin.zheng@corel.com>
2019-07-12 10:39:34 -0400

    Initial Commit

---
+++ MTGASIm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MTGASIm
+//
+//  Created by Haomin Zheng on 2019-07-12.
+//  Copyright © 2019 Haomin Zheng. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
